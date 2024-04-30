059f30db95816a332ef2dbb1ef99b182f37eb918
arrpppiiiittttt <123664577+arrpppiiiittttt@users.noreply.github.com>
2024-04-30 13:36:20 +0000

    02vitereact

---
+++ 02vitereact/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <chai/>
+    <h1>chai aur react</h1>
+    <p> test para</p>
+    </>
+  )
+}
+
+export default App
